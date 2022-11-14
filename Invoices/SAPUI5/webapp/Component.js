9d0c1478894530b22df5c81bb5ebdc23fbd34f7d
dfabyf66 <diegofabianf@yahoo.com>
2022-11-14 20:43:33 +0000

    TEMA 7

--- Invoices/SAPUI5/webapp/Component.js
+++ Invoices/SAPUI5/webapp/Component.js
@@ -1,15 +1,15 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
     "DFFspace/SAPUI5/model/Models",
-    "sap/ui/model/resource/ResourceModel"
+    "sap/ui/model/resource/ResourceModel",
+    "./controller/HelloDialog"
 
 ],
     /**
      * @param {typeof sap.ui.core.UIComponent} UIComponent 
-     * @param {DFFspace.}
      * @param {typeof sap.ui.core.model.resource.ResourceModel} ResourceModel
      */
-    function (UIComponent, Models, ResourceModel) {
+    function (UIComponent, Models, ResourceModel, HelloDialog) {
 
 
 
@@ -36,9 +36,21 @@ sap.ui.define([
                 // set i18n model on the view
                 var i18nModel = new ResourceModel({ bundleName: "DFFspace.SAPUI5.i18n.i18n" });
                 this.setModel(i18nModel, "i18n");
+                
+                this._helloDialog = new HelloDialog(this.getRootControl())
 
+            } ,
 
-            } 
+            exit: function() {
+                this._helloDialog.destroy();
+                delete this._helloDialog;
+
+            },
+
+            openHelloDialog: function() {
+                this._helloDialog().open();
+            }
+
 
         })
  }
