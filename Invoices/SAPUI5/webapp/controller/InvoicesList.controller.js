5a4b610d00698ab194cf7947a70f8f51b63554da
dfabyf66 <diegofabianf@yahoo.com>
2022-11-21 17:51:43 +0000

    Binding y Formateadores

--- Invoices/SAPUI5/webapp/controller/InvoicesList.controller.js
+++ Invoices/SAPUI5/webapp/controller/InvoicesList.controller.js
@@ -3,17 +3,18 @@ sap.ui.define([
     'sap/ui/core/mvc/Controller',
     'sap/ui/model/json/JSONModel',
     '../model/InvoicesFormatter',
-    // 'sap/ui/model/Filter',
+    'sap/ui/model/Filter',
+    'sap/ui/model/FilterOperator'
 ], 
 /**
  * 
  * @param { typeof sap.ui.core.mvc.Controller } Controller 
  * @param { typeof sap.ui.model.json.JSONModel } JSONModel
- * param { typeof sap.ui.model.Filter } Filter
- * param { typeof sap.ui.model.FilterOperator } FilterOperator
+ * @param { typeof sap.ui.model.Filter } Filter
+ * @param { typeof sap.ui.model.FilterOperator } FilterOperator
  *
  **/
-function (Controller, JSONModel, InvoicesFormatter) {
+function (Controller, JSONModel, InvoicesFormatter, Filter, FilterOperator) {
 
     return Controller.extend("DFFspace.SAPUI5.controller.InvoicesList", {
         
@@ -25,6 +26,20 @@ function (Controller, JSONModel, InvoicesFormatter) {
                 eur: "EUR"
             });
             this.getView().setModel(oViewModel, "currency");
+        },
+
+        onFilterInvoices : function(oEvent) {
+
+            const aFilter = [];
+            const sQuery = oEvent.getParameter("query");
+
+            if (sQuery) {
+              aFilter.push( new Filter("ProductName", FilterOperator.Contains, sQuery ));  
+            };
+
+            const oList = this.getView().byId("invoiceList");
+            const oBinding = oList.getBinding("items");
+            oBinding.filter(aFilter);
         }
 
     });
