9079d08b10e948c488a73dc577515077f63f60aa
dfabyf66 <diegofabianf@yahoo.com>
2022-11-24 16:48:18 +0000

    07_Interfaz_de_Usuario

--- Invoices/SAPUI5/webapp/Component.js
+++ Invoices/SAPUI5/webapp/Component.js
@@ -2,13 +2,14 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
     "DFFspace/SAPUI5/model/Models",
-    "sap/ui/model/resource/ResourceModel"
+    "sap/ui/model/resource/ResourceModel",
+    "./controller/HelloDialog"
 ],
     /**
      * @param {typeof sap.ui.core.UIComponent} UIComponent
      * @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel
      */
-    function (UIComponent, Models, ResourceModel) {
+    function (UIComponent, Models, ResourceModel,  HelloDialog) {
 
         return UIComponent.extend("DFFspace.SAPUI5.Component", {
 
@@ -26,7 +27,19 @@ sap.ui.define([
                 //set i18n model on the view
                 var i18nModel = new ResourceModel({ bundleName: "DFFspace.SAPUI5.i18n.i18n" });
                 this.setModel(i18nModel, "i18n");
-            }
+
+                this._helloDialog = new HelloDialog(this.getRootControl());
+            }, 
+
+            exit: function() {
+               this._helloDialog.destroy();
+               delete this._helloDialog;
+           },
+
+             openHelloDialog: function () {
+                 this._helloDialog.open();
+        
+             }
         });
 
     });
--- Invoices/SAPUI5/webapp/controller/App.controller.js
+++ Invoices/SAPUI5/webapp/controller/App.controller.js
@@ -16,13 +16,17 @@ sap.ui.define([
 
             },
 
-            onShowHello: function () {
-                //read text from i18n model
-                var oBundle = this.getView().getModel("i18n").getResourceBundle();
-                //read property from data model
-                var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-                var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-                MessageToast.show(sMsg);
+            onOpenDialogHeader: function() {
+                this.getOwnerComponent().openHelloDialog();
             }
+
+            // onShowHello: function () {
+            //     //read text from i18n model
+            //     var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            //     //read property from data model
+            //     var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            //     var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+            //     MessageToast.show(sMsg);
+            // }
         });
     });
