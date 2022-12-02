f36e4bdb355c9f3a49d1b88b9e92133860acdf00
dfabyf66 <diegofabianf@yahoo.com>
2022-12-02 14:01:12 +0000

    ODATA ANDANDO Invoices2

---
+++ Invoices2/SAPUI5/webapp/controller/App.controller.js
@@ -0,0 +1,22 @@
+// @ts-nocheck
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("DFFspace2.SAPUI5.controller.App", {
+
+            onInit: function () {
+
+            },
+
+            onOpenDialogHeader: function() {
+                this.getOwnerComponent().openHelloDialog();
+            }
+            
+        });
+    });
